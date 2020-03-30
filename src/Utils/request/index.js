ec3a81ad09398a7f8053d1d4c3739fb9186e7c31
buitrbao222 <57611063+buitrbao222@users.noreply.github.com>
2020-03-30 15:32:36 +0700

    Update

--- src/Utils/request/index.js
+++ src/Utils/request/index.js
@@ -1,10 +1,15 @@
 import useAxios from 'axios-hooks'
 
+import { getToken } from 'Utils/auth/index'
+
 export function useRequest({ method, api, data = {} }) {
   const [{data: fetched, error, loading}, refetch] = useAxios({
     url: process.env.REACT_APP_BACKEND + api,
     method,
-    data
+    data,
+    headers: {
+      Authorization: `Bearer ${getToken()}` || ''
+    }
   })
 
   return {data: fetched,error,loading,refetch}
