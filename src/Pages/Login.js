f6b6d43522784e5f6a3219b71c7a75395a05e9c8
buitrbao222 <57611063+buitrbao222@users.noreply.github.com>
2020-04-06 14:41:31 +0700

    Register form validation.

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,18 +1,19 @@
 import React, { useState, useRef } from 'react';
-import {setToken, isLogin} from '../Utils/auth'
-import { Redirect, Link } from 'react-router-dom'
-import { useRequest } from 'Utils/request'
+import { setToken, isLogin } from '../Utils/auth';
+import { Redirect, Link } from 'react-router-dom';
+import { useRequest } from 'Utils/request';
+
+import { Helmet } from 'react-helmet';
 
 import { useHistory } from 'react-router-dom';
 
-import { Icon } from '@iconify/react'; 
+import { Icon } from '@iconify/react';
 import accountCircle from '@iconify/icons-mdi/account-circle';
-import shieldLock from '@iconify/icons-mdi/shield-lock';
+import lockOpen from '@iconify/icons-mdi/lock-open';
 
 import 'SCSS/Form.scss';
 
 export default function Login() {
-
   const history = useHistory();
 
   // const {data, error, loading, refetch} = useRequest({
@@ -20,12 +21,12 @@ export default function Login() {
   //   method: "POST",
   //   data: {
   //     username: "admin",
-	//     password: "12345"
+  //     password: "12345"
   //   }
   // })
-  
+
   // if(isLogin()) return <Redirect push to="/"/>
-  
+
   // if(data){
   //   setToken(data)
   // }
@@ -39,37 +40,45 @@ export default function Login() {
         <Icon className="icon" icon={icon} />
         <input ref={inputRef} placeholder={placeholder} type="text"></input>
       </div>
-    )
+    );
   }
 
   function handleSubmit(e) {
     e.preventDefault();
-    const usernameValue = usernameRef.current.value;
-    const passwordValue = passwordRef.current.value;
+    const username = usernameRef.current.value;
+    const password = passwordRef.current.value;
+    console.log(username, password);
   }
 
   return (
     <div className="form-page">
+
+      <Helmet>
+        <title>Reviewz | Đăng nhập</title>
+      </Helmet>
+
       <div className="brand-name">Reviewz</div>
+
       <div className="form">
-        <div className="header">
-          Đăng nhập
-        </div>
+        <div className="header">Đăng nhập</div>
+
         <form onSubmit={handleSubmit} className="grid">
-          {inputRow(accountCircle, "Tên đăng nhập", usernameRef)}
-          {inputRow(shieldLock, "Mật khẩu", passwordRef)}
-          <button type="submit">
-            Đăng nhập
-          </button>
+          {inputRow(accountCircle, 'Tên đăng nhập', usernameRef)}
+
+          {inputRow(lockOpen, 'Mật khẩu', passwordRef)}
+
+          <button type="submit">Đăng nhập</button>
+
           <div className="alternate-link">
             <span>Chưa có tài khoản? </span>
             <Link to="/register">Đăng ký</Link>
           </div>
         </form>
+
         <Link to="/" className="back-to-home">
           <span> Trở về trang chủ</span>
         </Link>
       </div>
     </div>
-  )
+  );
 }
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,58 +1,128 @@
-import React, { useRef } from 'react';
-import { useHistory, Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Icon } from '@iconify/react'; 
+import { useForm } from 'react-hook-form';
+
+import { Helmet } from 'react-helmet';
+
+import * as yup from 'yup';
+
+import { Icon } from '@iconify/react';
 import accountCircle from '@iconify/icons-mdi/account-circle';
-import shieldLock from '@iconify/icons-mdi/shield-lock';
+import lockOpen from '@iconify/icons-mdi/lock-open';
+import lockOpenCheck from '@iconify/icons-mdi/lock-open-check';
+import alertCircleOutline from '@iconify/icons-mdi/alert-circle-outline';
 
 import 'SCSS/Form.scss';
 
 export default function About() {
-  const history = useHistory();
+  const { register, handleSubmit, errors, getValues } = useForm({
+    validationSchema: yup.object().shape({
+      username: yup
+        .string()
+        .required('Tên đăng nhập không được bỏ trống')
+        .test(
+          'numberFirst',
+          'Tên đăng nhập không được bắt đầu bằng số',
+          value => isNaN(value[0])
+        )
+        .matches(
+          /^[A-Za-z0-9]+$/,
+          'Tên đăng nhập chỉ được chứa chữ cái và chữ số'
+        )
+        .min(5, 'Tên đăng nhập tối thiểu 5 kí tự'),
 
-  const usernameRef = useRef();
-  const passwordRef = useRef();
-  const confirmPasswordRef = useRef();
+      password: yup
+        .string()
+        .required('Mật khẩu không được bỏ trống')
+        .matches(
+          /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)+$/,
+          'Mật khẩu phải có ít nhất 1 chữ cái, 1 chữ số và không có kí tự đặc biệt'
+        )
+        .min(6, 'Mật khẩu tối thiểu 6 kí tự'),
 
-  function inputRow(icon, placeholder, inputRef) {
+      confirm: yup
+        .string()
+        .test(
+          'matchPassword',
+          'Xác nhận mật khẩu không đúng',
+          value => value === getValues().password
+        ),
+    }),
+  });
+
+  function errorMessage(message) {
     return (
-      <div className="row">
-        <Icon className="icon" icon={icon} />
-        <input ref={inputRef} placeholder={placeholder} type="text"></input>
+      <div className="error-message">
+        <Icon className="icon" icon={alertCircleOutline} />
+        <div className="text"> {message}</div>
       </div>
-    )
+    );
   }
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    const username = usernameRef.current.value;
-    const password = passwordRef.current.value;
-    const confirmPassword = passwordRef.current.value;
+  function onSubmit(data) {
+    const { username, password, confirm } = data;
+
+    console.log(data);
   }
 
   return (
     <div className="form-page">
+      <Helmet>
+        <title>Reviewz | Đăng ký</title>
+      </Helmet>
+
       <div className="brand-name">Reviewz</div>
+
       <div className="form">
-        <div className="header">
-          Đăng ký
-        </div>
-        <form onSubmit={handleSubmit} className="grid">
-          {inputRow(accountCircle, "Tên đăng nhập", usernameRef)}
-          {inputRow(shieldLock, "Mật khẩu", passwordRef)}
-          {inputRow(shieldLock, "Xác nhận mật khẩu", confirmPasswordRef)}
-          <button type="submit">
-            Đăng ký
-          </button>
+        <div className="header">Đăng ký</div>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="grid">
+          <div className="row">
+            <Icon className="icon" icon={accountCircle} />
+            <input
+              ref={register}
+              name="username"
+              placeholder="Tên đăng nhập"
+              type="text"
+            ></input>
+            {errors.username && errorMessage(errors.username.message)}
+          </div>
+
+          <div className="row">
+            <Icon className="icon" icon={lockOpen} />
+            <input
+              ref={register}
+              name="password"
+              placeholder="Mật khẩu"
+              type="password"
+            ></input>
+            {errors.password && errorMessage(errors.password.message)}
+          </div>
+
+          <div className="row">
+            <Icon className="icon" icon={lockOpenCheck} />
+            <input
+              ref={register}
+              name="confirm"
+              placeholder="Xác nhận mật khẩu"
+              type="password"
+            ></input>
+            {errors.confirm && errorMessage(errors.confirm.message)}
+          </div>
+
+          <button type="submit">Đăng ký</button>
+
           <div className="alternate-link">
             <span>Đã có tài khoản? </span>
             <Link to="/login">Đăng nhập</Link>
           </div>
         </form>
+
         <Link to="/" className="back-to-home">
           <span> Trở về trang chủ</span>
         </Link>
       </div>
     </div>
-  )
+  );
 }
