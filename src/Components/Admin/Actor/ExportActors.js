dc459eff94b5e5c9555888f718ff5c1b81369d07
buitrbao222 <57611063+buitrbao222@users.noreply.github.com>
2020-06-21 22:22:49 +0700

    Added Export button on Admin Actors.

--- src/Components/Admin/Actor/ExportActors.js
+++ src/Components/Admin/Actor/ExportActors.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-export default function () {
-  return <div></div>;
+import fileExport from '@iconify/icons-mdi/file-export';
+import { IconButton } from 'Components/Shared/Buttons';
+
+export default function (props) {
+  const { gridApi } = props;
+
+  return (
+    <IconButton
+      onClick={() =>
+        gridApi.exportDataAsCsv({
+          columnKeys: ['name'],
+        })
+      }
+      icon={fileExport}
+      text="Xuất file"
+    />
+  );
 }
